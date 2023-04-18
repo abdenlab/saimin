dea6cdb6a268e566fd2369850eefe3fe866dd7a6
Trevor Manz <trevor.j.manz@gmail.com>
2023-04-18 09:25:46 -0400

    feat: use noodles

--- x.py
+++ x.py
@@ -9,7 +9,7 @@ import bram
 
 def main():
     with bram.BamReader(sys.argv[1]) as reader:
-        ipc = reader.fetch("chr2", 0, 1_000_000)
+        ipc = reader.fetch("chr2", 1, 1_000_000)
     df = polars.read_ipc(ipc)
     print(df)
 
