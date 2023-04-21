2bcc409471258c38a3989286f37fd80eccce93a6
Nezar Abdennur <nabdennur@gmail.com>
2023-04-21 13:27:35 -0400

    Change to buffered reader and add mapq field

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,11 @@ use noodles::bgzf;
 use noodles::core::{Position, Region};
 use noodles::sam;
 
+type BufferedReader = std::io::BufReader<std::fs::File>;
+
 #[pyclass]
 struct BamReader {
-    reader: bam::IndexedReader<bgzf::Reader<std::fs::File>>,
+    reader: bam::IndexedReader<bgzf::Reader<BufferedReader>>,
     header: sam::Header,
 }
 
@@ -26,15 +28,19 @@ struct BamReader {
 impl BamReader {
     #[new]
     fn py_new(path: &str) -> Self {
+        let index = bam::bai::read(format!("{}.bai", path)).unwrap();
+        let file = std::fs::File::open(path).unwrap();
+        let bufreader = std::io::BufReader::with_capacity(1024 * 1024, file);
         let mut reader = bam::indexed_reader::Builder::default()
-            .build_from_path(path)
+            .set_index(index)
+            .build_from_reader(bufreader)
             .unwrap();
         let header = reader.read_header().unwrap();
         Self { reader, header }
     }
 
     pub fn fetch(&mut self, chrom: &str, start: usize, end: usize) -> PyObject {
-        let start = Position::try_from(start).unwrap();
+        let start = Position::try_from(start + 1).unwrap();
         let end = Position::try_from(end).unwrap();
         let query = self
             .reader
@@ -42,6 +48,19 @@ impl BamReader {
             .unwrap();
         let ipc = write_ipc(query, &self.header);
         Python::with_gil(|py| PyBytes::new(py, &ipc).into())
+    }
+
+    pub fn fetch_noodles(&mut self, chrom: &str, start: usize, end: usize) -> () {
+        let start = Position::try_from(start + 1).unwrap();
+        let end = Position::try_from(end).unwrap();
+        let query = self
+            .reader
+            .query(&self.header, &Region::new(chrom, start..=end))
+            .unwrap();
+        for record in query {
+            let _record = record.unwrap();
+        }
+        print!("done");
     }
 
     /// https://github.com/PyO3/pyo3/issues/1205#issuecomment-1164096251 for advice on `__enter__`
@@ -52,13 +71,13 @@ impl BamReader {
     pub fn __exit__(&mut self, _exc_type: PyObject, _exc_value: PyObject, _traceback: PyObject) {}
 }
 
-fn write_ipc(query: bam::reader::Query<std::fs::File>, header: &sam::Header) -> Vec<u8> {
+fn write_ipc(query: bam::reader::Query<BufferedReader>, header: &sam::Header) -> Vec<u8> {
     let mut refs = StringDictionaryBuilder::<Int8Type>::new();
 
     let size = 100; // TODO: get size from region_viewer?
     let mut starts = Int32Array::builder(size);
     let mut ends = Int32Array::builder(size);
-
+    let mut mapqs = Int32Array::builder(size);
     let mut names = GenericBinaryBuilder::<i32>::new();
     let mut cigars = GenericBinaryBuilder::<i32>::new();
     let mut seqs = GenericBinaryBuilder::<i32>::new();
@@ -75,6 +94,7 @@ fn write_ipc(query: bam::reader::Query<std::fs::File>, header: &sam::Header) -> 
         refs.append(ref_name).unwrap();
         starts.append_value(record.alignment_start().unwrap().get() as i32);
         ends.append_value(record.alignment_end().unwrap().get() as i32);
+        mapqs.append_value(record.mapping_quality().unwrap().get() as i32);
         names.append_value(record.read_name().unwrap());
         cigars.append_value(record.cigar().to_string());
         seqs.append_value(record.sequence().to_string());
@@ -91,6 +111,7 @@ fn write_ipc(query: bam::reader::Query<std::fs::File>, header: &sam::Header) -> 
         ("ref", Arc::new(refs.finish()) as ArrayRef),
         ("start", Arc::new(starts.finish()) as ArrayRef),
         ("end", Arc::new(ends.finish()) as ArrayRef),
+        ("mapq", Arc::new(mapqs.finish()) as ArrayRef),
         ("name", Arc::new(names) as ArrayRef),
         ("cigar", Arc::new(cigars) as ArrayRef),
         ("seq", Arc::new(seqs) as ArrayRef),
